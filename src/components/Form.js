89549e449a962f96e0fddf8a77eb4e9864fe8eeb
pratdvn <81229684+pratdvn@users.noreply.github.com>
2023-10-27 09:17:39 +0530

    SDFG

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,28 +4,27 @@ import Nav from './Nav2';
 import bgImp from '../images/bckrnd.png';
 
 export default function Form() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-
-    
   return (
     <section>
-        <div className="register">
-            <div className='col-2'>
-                <img src={bgImp} alt='' />
-            </div>
-            <div className="col-1">
-                <h2>Sign In</h2>
-
-                <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-                    <input type="text" {...register("username")} placeholder='Username' />
-                    <input type="text" {...register("password")} placeholder='Password' />
-                    <button className='btn' onClick={<Nav/>}>Sign In</button>
-                </form>
-
-            </div>
+      <div className="register">
+        <div className="left-col">
+          <img src={bgImp} alt='' />
         </div>
+        <div className="right-col">
+          <div className="centered">
+            <h2>Sign In</h2>
+          </div>
+
+          <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" {...register("username")} placeholder='Username' />
+            <input type="text" {...register("password")} placeholder='Password' />
+            <button className='btn' onClick={<Nav />}>Sign In</button>
+          </form>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
